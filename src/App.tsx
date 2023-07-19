b07565e424757da0f68b7ff29d2f2763d00ebdee
Lukas Karberg <deaderlp@googlemail.com>
2023-07-19 15:07:34 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.scss";
+
+function App() {
+  return (
+    /*html*/ <>
+      <main className="container flex flex-col h-screen  items-center text-center ">
+        <section className="container mt-48 mb-24">
+          <h1 className="text-cyan-200 text-7xl">This is a daisy ui site</h1>
+        </section>
+        <section className="container w-[50%]">
+          <h2>This is a second section.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            libero sapiente, repellendus quaerat error soluta blanditiis aut
+            minus eaque esse!
+          </p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
