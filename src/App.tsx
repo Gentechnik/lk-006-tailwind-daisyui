e16e8bc16b02828874ea7bdf76440789abbabd6f
Lukas Karberg <deaderlp@googlemail.com>
2023-07-19 15:38:48 +0200

    Lukas: Played around with new Components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.scss";
 function App() {
   return (
     /*html*/ <>
-      <main className="container flex flex-col h-screen  items-center text-center ">
+      <main className=" flex flex-col h-screen w-screen items-center text-center ">
         <section className="container mt-48 mb-24">
           <h1 className="text-cyan-200 text-7xl">This is a daisy ui site</h1>
         </section>
@@ -14,6 +14,41 @@ function App() {
             libero sapiente, repellendus quaerat error soluta blanditiis aut
             minus eaque esse!
           </p>
+          <div className="grid gap-6 grid-cols-4 grid-rows-1 mt-6 mb-12">
+            <button className="btn btn-outline grid-cols-1 row-span-1">
+              Default
+            </button>
+            <button className="btn btn-outline btn-primary grid-cols-2 row-span-1">
+              Primary
+            </button>
+            <button className="btn btn-outline btn-secondary cols-3 row-span-1">
+              Secondary
+            </button>
+            <button className="btn btn-outline btn-accent cols-4 row-span-1">
+              Accent
+            </button>
+          </div>
+
+          <button className="btn text-3xl m-7">
+            Money
+            <div className="badge badge-secondary h-14 text-4xl">-150</div>
+          </button>
+          <input
+            type="range"
+            min={0}
+            max="100"
+            value="80"
+            className="range range-success bg-red-600"
+          />
+        </section>
+        <section>
+          <label className="swap swap-flip text-9xl">
+            {/* this hidden checkbox controls the state */}
+            <input type="checkbox" />
+
+            <div className="swap-on">🥶</div>
+            <div className="swap-off">🧊</div>
+          </label>
         </section>
       </main>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,13 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "cyberpunk"],
+  },
 };
+// module.exports = {
+//   //...
+//   daisyui: {
+//     themes: ["light", "dark", "cupcake"],
+//   },
+// };
